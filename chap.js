a2d9ef9cb88aa17bb421fdd19e6b8666cd6791ab
vtung <116045776+vtungo7@users.noreply.github.com>
2025-05-04 13:13:00 +0700

    Delete chap.js

--- chap.js
+++
@@ -1,6 +0,0 @@
-function execute(url) {
-    var doc = Http.get(url).html();
-    var imgs = []
-    doc.select('.reading-content img').forEach(e => imgs.push(e.attr("src")));
-    return Response.success(imgs);
-}
